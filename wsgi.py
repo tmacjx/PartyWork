bb6ccaaea37e4f374b2f850bf6a2df6f17ad856e
tmacjx <381221780@qq.com>
2017-04-30 15:49:58 +0800

    fix

--- wsgi.py
+++ wsgi.py
@@ -2,16 +2,8 @@
 """
 Author:
 """
-# import ssl
-
 
 from app import create_app
-
-
-# context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-# context.load_cert_chain('./ssl/server.crt', './ssl/server.rsa')
-# context.use_privatekey_file('./ssl/server.rsa')
-# context.use_certificate_file('./ssl/server.crt')
 
 if __name__ == "__main__":
 
